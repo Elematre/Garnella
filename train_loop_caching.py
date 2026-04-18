29e994f2bb39fbd24302d5f0bc3c7185d9e63a75
Tae <babypoby@gmail.com>
2026-04-18 02:11:55 +0200

    big changes ?? i guess

--- train_loop_caching.py
+++ train_loop_caching.py
@@ -26,13 +26,14 @@ def train_loop(train_data, test_data, combinations):
         Y_val_pred = model.predict(X_val)
         score_train, mae_train, accuracy_train, score_val, mae_val, accuracy_val = evaluateMAE(Y_train_pred, Y_train, Y_val_pred, Y_val)
 
-        print(f"Combination: {func_name} + {type(model).__name__}")
+        
+        print(f"Combination: {func_name} + {model_func.__name__}")
         print(f"Training Score: {score_train:.4f}, MAE: {mae_train:.4f}, Accuracy: {accuracy_train:.4f}")
         print(f"Validation Score: {score_val:.4f}, MAE: {mae_val:.4f}, Accuracy: {accuracy_val:.4f}")
 
         results.append({
             "vectorizer": func_name,
-            "model": type(model).__name__,
+            "model": model_func.__name__,
             "training_score": score_train,
             "training_mae": mae_train,
             "training_accuracy": accuracy_train,
